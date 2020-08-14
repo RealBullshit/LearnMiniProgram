5ebb99a727c75d4448db78e676026a7929cff70f
RealBullshit <2365231058.com>
2020-08-14 09:27:13 +0800

    02_全局配置和局部配置

---
+++ pages/category/category.js
@@ -0,0 +1,12 @@
+// pages/category/category.js
+Page({
+  data: {
+    message: '哈哈哈'
+  },
+  handleBtnClick() {
+    // console.log('按钮发生了点击')
+    this.setData({
+      message: '呵呵呵'
+    })
+  }
+})
